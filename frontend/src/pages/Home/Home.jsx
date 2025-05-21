b0d984904ef9ed627b410981b70c725fe106c83c
tigistu <twoo53591@gmail.com.com>
2025-05-21 12:15:00 +0300

    new update3

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
-import FoodDisplay from "../../components/foodDlisplay/FoodDisplay";
+import FoodDisplay from "../../components/FoodDlisplay/FoodDisplay";
 import AppDownload from "../../components/AppDownload/AppDownload";
 
 const Home = () => {
