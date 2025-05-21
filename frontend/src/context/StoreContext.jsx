294b6bdc39ef961e548a57ec74cb7bb5e687bc31
tigistu <twoo53591@gmail.com.com>
2025-05-21 12:05:03 +0300

    new update2

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = import.meta.env.VITE_BACKEND_URL;
+
   const [token, setToken] = useState("");
   const [food_list, setFood_list] = useState([]);
 
