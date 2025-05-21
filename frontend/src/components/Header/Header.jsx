5524c67a389aa61f414bc846e5ca8d5b9db3f729
tigistu <twoo53591@gmail.com.com>
2025-05-21 11:34:18 +0300

    new update1

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 const Header = () => {
-    return (
-        <div className='header'>
-            <div className="header-contents">
-                <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our </p>
-                <button>View Menu</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food from Ethiopia</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients and culinary expertise. Our{" "}
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
